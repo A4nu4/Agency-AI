462af4312400a5bfd0098beef1f4c64c5d356199
Arnur Omirserikov <arnuromirserikov@Arnurs-MacBook-Air.local>
2026-09-03 15:37:07 +0500

    Added Contact Us, Our Work and Teams sections

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import Navbar from "@src/components/Navbar";
 import Hero from "./components/Hero";
 import TrustedBy from "./components/TrustedBy";
 import Services from "./components/Services";
+import OurWork from "./components/OurWork";
+import Teams from "./components/Teams";
+import ContactUs from "./components/ContactUs";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
   const [theme, setTheme] = React.useState(
@@ -11,10 +15,14 @@ const App = () => {
 
   return (
     <div className="dark:bg-black relative">
+      <Toaster />
       <Navbar theme={theme} setTheme={setTheme} />
       <Hero />
       <TrustedBy />
       <Services />
+      <OurWork />
+      <Teams />
+      <ContactUs />
     </div>
   );
 };
